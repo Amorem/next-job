fa1a891692ab53ccdc86c8a800a944acc7b488c5
Amorem <dev@amorem.com>
2025-01-29 21:58:15 +0100

    createCompany action

---
+++ app/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { requireUser } from "@/lib/requireUser";
+import { companySchema } from "@/lib/zodSchemas";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+export async function createCompany(data: z.infer<typeof companySchema>) {
+	const session = await requireUser();
+	const validateData = companySchema.parse(data);
+
+	await prisma.user.update({
+		where: {
+			id: session.id,
+		},
+		data: {
+			onBoardingCompleted: true,
+			userType: "COMPANY",
+			Company: {
+				create: {
+					...validateData,
+				},
+			},
+		},
+	});
+	return redirect("/");
+}
--- components/forms/onboarding/CompanyForm.tsx
+++ components/forms/onboarding/CompanyForm.tsx
@@ -3,167 +3,188 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { companySchema } from "@/lib/zodSchemas";
 import { z } from "zod";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
 import { SelectGroup } from "@radix-ui/react-select";
 import { countryList } from "@/lib/countriesList";
 import { Textarea } from "@/components/ui/textarea";
 import { UploadDropzone } from "@/components/general/UploadThingReexported";
+import { createCompany } from "@/app/actions";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export function CompanyForm() {
-  const form = useForm<z.infer<typeof companySchema>>({
-    resolver: zodResolver(companySchema),
-    defaultValues: {
-      about: "",
-      location: "",
-      logo: "",
-      name: "",
-      website: "",
-      xAccount: "",
-    },
-  });
+	const form = useForm<z.infer<typeof companySchema>>({
+		resolver: zodResolver(companySchema),
+		defaultValues: {
+			about: "",
+			location: "",
+			logo: "",
+			name: "",
+			website: "",
+			xAccount: "",
+		},
+	});
 
-  return (
-    <Form {...form}>
-      <form className="space-y-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Company Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter company name" {...field}></Input>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="location"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Company Location</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select location"></SelectValue>
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Worldwide</SelectLabel>
-                      <SelectItem value="worldwide">
-                        <span>🌍</span> <span>Worldwide / Remote</span>
-                      </SelectItem>
-                    </SelectGroup>
-                    <SelectGroup>
-                      <SelectLabel>Location</SelectLabel>
-                      {countryList.map((country) => (
-                        <SelectItem value={country.name} key={country.code}>
-                          <span>{country.flagEmoji}</span>
-                          <span className="pl-2">{country.name}</span>
-                        </SelectItem>
-                      ))}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <FormField
-            control={form.control}
-            name="website"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Website</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="https://yourcompany.com"
-                    {...field}
-                  ></Input>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+	const [pending, setPending] = useState(false);
 
-          <FormField
-            control={form.control}
-            name="xAccount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>X (Twitter) Account</FormLabel>
-                <FormControl>
-                  <Input placeholder="@yourcompany" {...field}></Input>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <FormField
-          control={form.control}
-          name="about"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>About</FormLabel>
-              <FormControl>
-                <Textarea
-                  {...field}
-                  placeholder="Tell us about your company..."
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="logo"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Company Logo</FormLabel>
-              <FormControl>
-                <UploadDropzone
-                  endpoint="imageUploader"
-                  onClientUploadComplete={(res) => {
-                    field.onChange(res[0].url);
-                    console.log("upload result", res);
-                  }}
-                  onUploadError={(error) => {
-                    console.log("Upload error", error);
-                  }}
-                  className="ut-button:bg-primary ut-button:text-white ut-button:hover:bg-primary/90 ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
-  );
+	async function onSubmit(data: z.infer<typeof companySchema>) {
+		try {
+			setPending(true);
+			await createCompany(data);
+		} catch (error) {
+			if (error instanceof Error && error.message !== "NEXT_REDIRECT") {
+				console.log("Something went wrong");
+			}
+		} finally {
+			setPending(false);
+		}
+	}
+
+	return (
+		<Form {...form}>
+			<form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+					<FormField
+						control={form.control}
+						name="name"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Company Name</FormLabel>
+								<FormControl>
+									<Input placeholder="Enter company name" {...field}></Input>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="location"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Company Location</FormLabel>
+								<Select
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+								>
+									<FormControl>
+										<SelectTrigger>
+											<SelectValue placeholder="Select location"></SelectValue>
+										</SelectTrigger>
+									</FormControl>
+									<SelectContent>
+										<SelectGroup>
+											<SelectLabel>Worldwide</SelectLabel>
+											<SelectItem value="worldwide">
+												<span>🌍</span> <span>Worldwide / Remote</span>
+											</SelectItem>
+										</SelectGroup>
+										<SelectGroup>
+											<SelectLabel>Location</SelectLabel>
+											{countryList.map((country) => (
+												<SelectItem value={country.name} key={country.code}>
+													<span>{country.flagEmoji}</span>
+													<span className="pl-2">{country.name}</span>
+												</SelectItem>
+											))}
+										</SelectGroup>
+									</SelectContent>
+								</Select>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+					<FormField
+						control={form.control}
+						name="website"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Website</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="https://yourcompany.com"
+										{...field}
+									></Input>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="xAccount"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>X (Twitter) Account</FormLabel>
+								<FormControl>
+									<Input placeholder="@yourcompany" {...field}></Input>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<FormField
+					control={form.control}
+					name="about"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>About</FormLabel>
+							<FormControl>
+								<Textarea
+									{...field}
+									placeholder="Tell us about your company..."
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="logo"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Company Logo</FormLabel>
+							<FormControl>
+								<UploadDropzone
+									endpoint="imageUploader"
+									onClientUploadComplete={(res) => {
+										field.onChange(res[0].url);
+										console.log("upload result", res);
+									}}
+									onUploadError={(error) => {
+										console.log("Upload error", error);
+									}}
+									className="ut-button:bg-primary ut-button:text-white ut-button:hover:bg-primary/90 ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit" className="w-full" disabled={pending}>
+					{pending ? "Submitting..." : "Continue"}
+				</Button>
+			</form>
+		</Form>
+	);
 }
