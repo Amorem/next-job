e5cdf367d3bab9685ea97d2436f87a523006c410
AMOREM <dev@amorem.com>
2025-01-29 16:15:59 +0100

    before uploadthing integration

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -3,63 +3,70 @@ import Github from "../icons/GitHub";
 import Google from "../icons/Google";
 
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "../ui/card";
 import GeneralSubmitButton from "../general/SubmitButtons";
 import { redirect } from "next/navigation";
 
 export default async function LoginForm() {
-	const session = await auth();
-	// console.log("session", session);
+  const session = await auth();
+  // console.log("session", session);
 
-	if (session?.user) {
-		return redirect("/");
-	}
-	return (
-		<div className="flex flex-col gap-6">
-			<Card>
-				<CardHeader className="text-center">
-					<CardTitle className="text-xl">Welcome Back</CardTitle>
-					<CardDescription>
-						Login with your Google or GitHub account
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<div className="flex flex-col gap-4">
-						<form
-							action={async () => {
-								"use server";
-								await signIn("github", {
-									redirectTo: "/",
-								});
-							}}
-						>
-							<GeneralSubmitButton
-								text="Login with GitHub"
-								variant={"outline"}
-								width="w-full"
-								icon={<Github />}
-							/>
-						</form>
-						<form>
-							<GeneralSubmitButton
-								text="Login with Google"
-								variant={"outline"}
-								width="w-full"
-								icon={<Google />}
-							/>
-						</form>
-					</div>
-				</CardContent>
-			</Card>
-			<div className="text-center text-xs text-muted-foreground text-balance">
-				By clicking continue, you agree to our terms, service, and privacy
-				policy
-			</div>
-		</div>
-	);
+  if (session?.user) {
+    return redirect("/");
+  }
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome Back</CardTitle>
+          <CardDescription>
+            Login with your Google or GitHub account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-4">
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github", {
+                  redirectTo: "/",
+                });
+              }}
+            >
+              <GeneralSubmitButton
+                text="Login with GitHub"
+                variant={"outline"}
+                width="w-full"
+                icon={<Github />}
+              />
+            </form>
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google", {
+                  redirectTo: "/",
+                });
+              }}
+            >
+              <GeneralSubmitButton
+                text="Login with Google"
+                variant={"outline"}
+                width="w-full"
+                icon={<Google />}
+              />
+            </form>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-center text-xs text-muted-foreground text-balance">
+        By clicking continue, you agree to our terms, service, and privacy
+        policy
+      </div>
+    </div>
+  );
 }
---
+++ components/forms/onboarding/CompanyForm.tsx
@@ -0,0 +1,145 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema } from "@/lib/zodSchemas";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { SelectGroup } from "@radix-ui/react-select";
+import { countryList } from "@/lib/countriesList";
+import { Textarea } from "@/components/ui/textarea";
+
+export function CompanyForm() {
+  const form = useForm<z.infer<typeof companySchema>>({
+    resolver: zodResolver(companySchema),
+    defaultValues: {
+      about: "",
+      location: "",
+      logo: "",
+      name: "",
+      website: "",
+      xAccount: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter company name" {...field}></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Location</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select location"></SelectValue>
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Worldwide</SelectLabel>
+                      <SelectItem value="worldwide">
+                        <span>🌍</span> <span>Worldwide / Remote</span>
+                      </SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Location</SelectLabel>
+                      {countryList.map((country) => (
+                        <SelectItem value={country.name} key={country.code}>
+                          <span>{country.flagEmoji}</span>
+                          <span className="pl-2">{country.name}</span>
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="website"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Website</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="https://yourcompany.com"
+                    {...field}
+                  ></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="xAccount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>X (Twitter) Account</FormLabel>
+                <FormControl>
+                  <Input placeholder="@yourcompany" {...field}></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="about"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>About</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  placeholder="Tell us about your company..."
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
